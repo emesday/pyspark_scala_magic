0386646f647edb572cb75e57689e0232d2a5316f
emeth.kim <emeth.kim@kakaocorp.com>
2019-09-06 14:33:34 +0900

    add requirements

--- setup.py
+++ setup.py
@@ -3,4 +3,9 @@ from setuptools import setup
 setup(
     name="pyspark_scala_magic",
     version="0.0.1",
-    packages=["pyspark_scala_magic"])
+    packages=["pyspark_scala_magic"],
+    install_requires=[
+        'spylon',
+        'spylon-kernel',
+        'jinja2'
+    ])
